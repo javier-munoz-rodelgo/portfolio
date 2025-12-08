32bf809b252e6ca43e5f27f00194bd446615b548
Javier Muñoz Rodelgo <javiermr2510@hotmail.com>
2025-12-08 22:52:22 +0100

    feat: Apply Inter font to the document body.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,10 @@
 import "./../styles/globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export default function RootLayout({
   children,
@@ -7,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body className="bg-white text-gray-900 antialiased">
+      <body className={`${inter.className} bg-white text-gray-900 antialiased`}>
         {children}
         {/* Aquí tu Footer */}
       </body>

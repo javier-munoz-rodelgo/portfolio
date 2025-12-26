92c58700955c070f4ed69ad0b72f0bc833a177f7
Javier Muñoz Rodelgo <javiermr2510@hotmail.com>
2025-12-26 19:12:30 +0100

    feat: add projects

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,29 +1,11 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { projects } from "@/data/projects";
+import Image from "next/image";
 
-const projects = [
-  {
-    title: "Dashboard Analytics",
-    desc: "Next.js + API + Charts",
-    image: "/images/p1.jpg",
-  },
-  {
-    title: "Landing App",
-    desc: "UI moderna con animaciones",
-    image: "/images/p2.jpg",
-  },
-  {
-    title: "Ecommerce Light",
-    desc: "Tienda minimalista",
-    image: "/images/p3.jpg",
-  },
-  {
-    title: "Portfolio Clean",
-    desc: "Diseño limpio y rápido",
-    image: "/images/p4.jpg",
-  },
-];
+// TODO: Añadir proyectos.
+// TODO: Añadir todas las secciones nuevas de los proyectos como tags.
 
 const container = {
   hidden: { opacity: 0 },
@@ -50,18 +32,33 @@ export default function Projects() {
         viewport={{ once: true }}
         className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10"
       >
-        {projects.map((p, i) => (
-          <motion.div
+        {projects?.map((p, i) => (
+          <motion.a
+            href={p.link}
+            target="_blank"
             key={i}
             variants={item}
-            className="bg-white rounded-xl shadow hover:shadow-lg transition overflow-hidden"
+            className="bg-white rounded-xl shadow hover:shadow-lg transition overflow-hidden border-1 border-gray-200 rounded-xl"
           >
-            <img src={p.image} className="w-full h-48 object-cover" />
+            <Image
+              alt={p.title}
+              src={p.image}
+              className="w-full h-48 object-contain border-b-1 border-gray-200 p-5"
+              width={500}
+              height={500}
+            />
+
             <div className="p-5">
-              <h3 className="text-xl font-semibold">{p.title}</h3>
-              <p className="text-gray-600">{p.desc}</p>
+              <div className="flex justify-between items-center mb-2">
+                <h3 className="text-xl font-semibold">{p.title}</h3>
+
+                <span className="text-gray-600 bg-purple-200 px-2 py-1 rounded-full text-xs">
+                  {p.tags.join(", ")}
+                </span>
+              </div>
+              <p className="text-gray-600">{p.description}</p>
             </div>
-          </motion.div>
+          </motion.a>
         ))}
       </motion.div>
     </section>
--- data/projects.ts
+++ data/projects.ts
@@ -1,42 +1,95 @@
-export const projects = [
+/**
+ * Modelo para definir los proyectos
+ */
+export interface Project {
+  title: string;
+  description: string;
+  tags: string[];
+  image: string;
+  link: string;
+}
+
+/**
+ * Listado de todos los proyectos.
+ */
+export const projects: Project[] = [
   {
-    title: "PulseDash",
+    title: "IRIS",
     description:
-      "Dashboard interactivo para monitorizar métricas de negocio en tiempo real. Desarrollado con Next.js, TypeScript y Chart.js.",
-    tags: ["Next.js", "TypeScript", "Chart.js", "API Integration"],
-    image: "/images/projects/pulsedash.png",
-    link: "https://pulsedash.dev",
+      "IRIS Web Componentes es una arquitectura moderna basada en un monorepo gestionado con Lerna, que centraliza todos los componentes visuales del ecosistema IRIS en paquetes independientes, reutilizables y escalables, desarrollados con Stencil.",
+    tags: ["Profesional"],
+    image: "/projects/iris.svg",
+    link: "https://iris.atm-maggioli.es/",
   },
   {
-    title: "DocuFlow",
+    title: "Apps móviles",
     description:
-      "Aplicación web para crear y compartir documentación técnica colaborativa, con soporte para Markdown y control de versiones.",
-    tags: ["React", "Node.js", "MongoDB", "Markdown"],
-    image: "/images/projects/docuflow.png",
-    link: "https://docuflow.app",
+      "Soluciones para la gestión de expedientes, pagos y consultas en la nube.",
+    tags: ["Profesional"],
+    image: "/projects/atm.png",
+    link: "https://play.google.com/store/apps/dev?id=8222897177929282618&hl=es_419",
   },
   {
-    title: "Portify",
+    title: "Sede electrónica",
     description:
-      "Generador de portfolios personales en línea basado en plantillas, optimizado para SEO y rendimiento.",
-    tags: ["Next.js", "TailwindCSS", "SEO", "Static Generation"],
-    image: "/images/projects/portify.png",
-    link: "https://portify.io",
+      "Plataforma para el ciudadano donde consultar el estado de sus Expedientes, Pagar tasas y recibos de impuestos (Oficina Virtual Tributaria), realizar gestiones relacionadas con el Padrón Municipal de Habitantes, Seguimiento del Perfil del Contratante, Registros automáticos etc.",
+    tags: ["Profesional"],
+    image: "/projects/atm.png",
+    link: "https://www.atm-maggioli.es/software-y-consultoria/administracion-electronica/",
   },
   {
-    title: "TaskNest",
+    title: "Gestión Integral de Tributos",
     description:
-      "Gestor de tareas con drag & drop y sincronización en tiempo real.",
-    tags: ["React", "Firebase", "Framer Motion", "UX Design"],
-    image: "/images/projects/tasknest.png",
-    link: "https://tasknest.app",
+      "Plataforma de Software en la nube para la gestión tributaria, recaudación voluntaria y ejecutiva, inspección y multas..",
+    tags: ["Profesional"],
+    image: "/projects/atm.png",
+    link: "https://www.atm-maggioli.es/software-y-consultoria/gestion-tributaria-y-recaudacion/",
   },
   {
-    title: "GreenPath",
-    description:
-      "Web para calcular la huella de carbono personal y ofrecer consejos para reducirla.",
-    tags: ["Next.js", "API Routes", "TailwindCSS", "Accessibility"],
-    image: "/images/projects/greenpath.png",
-    link: "https://greenpath.life",
+    title: "Vithas Hospitales",
+    description: "Cuadro médico, gestión de citas y portal del paciente.",
+    tags: ["Profesional"],
+    image: "/projects/vithas.svg",
+    link: "https://vithas.es/",
   },
+  // {
+  //   title: "PulseDash",
+  //   description:
+  //     "Dashboard interactivo para monitorizar métricas de negocio en tiempo real. Desarrollado con Next.js, TypeScript y Chart.js.",
+  //   tags: ["Next.js", "TypeScript", "Chart.js", "API Integration"],
+  //   image: "/images/projects/pulsedash.png",
+  //   link: "https://pulsedash.dev",
+  // },
+  // {
+  //   title: "DocuFlow",
+  //   description:
+  //     "Aplicación web para crear y compartir documentación técnica colaborativa, con soporte para Markdown y control de versiones.",
+  //   tags: ["React", "Node.js", "MongoDB", "Markdown"],
+  //   image: "/images/projects/docuflow.png",
+  //   link: "https://docuflow.app",
+  // },
+  // {
+  //   title: "Portify",
+  //   description:
+  //     "Generador de portfolios personales en línea basado en plantillas, optimizado para SEO y rendimiento.",
+  //   tags: ["Next.js", "TailwindCSS", "SEO", "Static Generation"],
+  //   image: "/images/projects/portify.png",
+  //   link: "https://portify.io",
+  // },
+  // {
+  //   title: "TaskNest",
+  //   description:
+  //     "Gestor de tareas con drag & drop y sincronización en tiempo real.",
+  //   tags: ["React", "Firebase", "Framer Motion", "UX Design"],
+  //   image: "/images/projects/tasknest.png",
+  //   link: "https://tasknest.app",
+  // },
+  // {
+  //   title: "GreenPath",
+  //   description:
+  //     "Web para calcular la huella de carbono personal y ofrecer consejos para reducirla.",
+  //   tags: ["Next.js", "API Routes", "TailwindCSS", "Accessibility"],
+  //   image: "/images/projects/greenpath.png",
+  //   link: "https://greenpath.life",
+  // },
 ];

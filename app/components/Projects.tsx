7fe1572e90c1efeef1a4fe347b0146edb779077c
Javier Muñoz Rodelgo <javiermr2510@hotmail.com>
2026-01-02 13:00:01 +0100

    refactor: remove commented-out project data and optional chaining from projects iteration

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -32,7 +32,7 @@ export default function Projects() {
         viewport={{ once: true }}
         className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10"
       >
-        {projects?.map((p, i) => (
+        {projects.map((p, i) => (
           <motion.a
             href={p.link}
             target="_blank"
